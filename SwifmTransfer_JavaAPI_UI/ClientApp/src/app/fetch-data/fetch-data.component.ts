d9abe2d7b8f3b52e72ba4f8fae1fcddf6609b37a
anant3007 <46474294+anant3007@users.noreply.github.com>
2019-03-22 14:44:47 +0530

    Created  Branch for Java API testing.

--- SwifmTransfer_JavaAPI_UI/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ SwifmTransfer_JavaAPI_UI/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,23 +1,22 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
+  public transactionHistory: any;
+  baseUrl = "https://swiftmtransferapi.azurewebsites.net/api";
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
+  constructor(private http: HttpClient) {
+    http.get<any>(this.baseUrl + "/User/GetAllTransactions").subscribe(
+      result => {
+        console.log(result);
+        this.transactionHistory = result;
+      },
+      error => console.error(error));
   }
-}
 
-interface WeatherForecast {
-  dateFormatted: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
 }
